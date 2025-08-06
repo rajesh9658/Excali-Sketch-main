85715506cd193ddec6b592ac1b8f60e0f5d39611
rajesh9658 <rajeshpadhi9658@gmail.com>
2025-08-06 17:44:10 +0530

    some changes

--- apps/ExcaliSketch/src/app/config.ts
+++ apps/ExcaliSketch/src/app/config.ts
@@ -2,8 +2,10 @@ import dotenv from "dotenv";
 import path from "path";
 
 dotenv.config();
-console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
+// console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
 export const BACKEND_URL =
   (process.env.NEXT_PUBLIC_BACKEND_URL as string) || "http://localhost:5000";
 export const WS_URL =
   (process.env.NEXT_PUBLIC_BACKEND_URL as string) || "http://localhost:5000";
+
+//apps/ExcaliSketch/src/app/config.ts
